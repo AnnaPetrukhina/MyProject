1f9da5bc536e1b57aa32062adbe54c4b089b7695
Аня Петрухина <apple1996@list.ru>
2020-08-31 19:13:43 +0300

    генератор NOK

--- log/log_parser2.py
+++ log/log_parser2.py
@@ -37,6 +37,31 @@ class Counter:
         yield self.line, self.count
 
 
-grouped_events = Counter("events.txt")
+# grouped_events = Counter("events.txt")
+# for group_time, event_count in grouped_events:
+#     print(f'{group_time}] {event_count}')
+
+
+def grouped(file_name):
+    count_nok = 1
+    with open(file_name, 'r') as ff:
+        for line in ff:
+            if line[-4:-1] == "NOK":
+                day = line.find(":") + 3
+                lin = line[:day]
+                break
+        for line in ff:
+            if line[-4:-1] == "NOK":
+                day = line.find(":") + 3
+                now_line = line[:day]
+                if now_line != lin:
+                    yield lin, count_nok
+                    count_nok = 0
+                    lin = now_line
+                count_nok += 1
+    yield lin, count_nok
+
+
+grouped_events = grouped("events.txt")
 for group_time, event_count in grouped_events:
     print(f'{group_time}] {event_count}')
