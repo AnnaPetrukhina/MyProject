5cfe21f626d9cbf0edc31c829ea6eef8dfa5dc45
Аня Петрухина <apple1996@list.ru>
2020-09-02 18:45:05 +0300

    расчет волатильности

---
+++ trading/volatility.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+
+
+# Описание предметной области:
+#
+# При торгах на бирже совершаются сделки - один купил, второй продал.
+# Покупают и продают ценные бумаги (акции, облигации, фьючерсы, етс). Ценные бумаги - это по сути долговые расписки.
+# Ценные бумаги выпускаются партиями, от десятка до несколько миллионов штук.
+# Каждая такая партия (выпуск) имеет свой торговый код на бирже - тикер - https://goo.gl/MJQ5Lq
+# Все бумаги из этой партии (выпуска) одинаковы в цене, поэтому говорят о цене одной бумаги.
+# У разных выпусков бумаг - разные цены, которые могут отличаться в сотни и тысячи раз.
+# Каждая биржевая сделка характеризуется:
+#   тикер ценнной бумаги
+#   время сделки
+#   цена сделки
+#   обьем сделки (сколько ценных бумаг было куплено)
+#
+# В ходе торгов цены сделок могут со временем расти и понижаться. Величина изменения цен называтея волатильностью.
+# Например, если бумага №1 торговалась с ценами 11, 11, 12, 11, 12, 11, 11, 11 - то она мало волатильна.
+# А если у бумаги №2 цены сделок были: 20, 15, 23, 56, 100, 50, 3, 10 - то такая бумага имеет большую волатильность.
+# Волатильность можно считать разными способами, мы будем считать сильно упрощенным способом -
+# отклонение в процентах от полусуммы крайних значений цены за торговую сессию:
+#   полусумма = (максимальная цена + минимальная цена) / 2
+#   волатильность = ((максимальная цена - минимальная цена) / полусумма) * 100%
+# Например для бумаги №1:
+#   half_sum = (12 + 11) / 2 = 11.5
+#   volatility = ((12 - 11) / half_sum) * 100 = 8.7%
+# Для бумаги №2:
+#   half_sum = (100 + 3) / 2 = 51.5
+#   volatility = ((100 - 3) / half_sum) * 100 = 188.34%
+#
+# В реальности волатильность рассчитывается так: https://goo.gl/VJNmmY
+#
+# Задача: вычислить 3 тикера с максимальной и 3 тикера с минимальной волатильностью.
+# Бумаги с нулевой волатильностью вывести отдельно.
+# Результаты вывести на консоль в виде:
+#   Максимальная волатильность:
+#       ТИКЕР1 - ХХХ.ХХ %
+#       ТИКЕР2 - ХХХ.ХХ %
+#       ТИКЕР3 - ХХХ.ХХ %
+#   Минимальная волатильность:
+#       ТИКЕР4 - ХХХ.ХХ %
+#       ТИКЕР5 - ХХХ.ХХ %
+#       ТИКЕР6 - ХХХ.ХХ %
+#   Нулевая волатильность:
+#       ТИКЕР7, ТИКЕР8, ТИКЕР9, ТИКЕР10, ТИКЕР11, ТИКЕР12
+# Волатильности указывать в порядке убывания. Тикеры с нулевой волатильностью упорядочить по имени.
+#
+# Подготовка исходных данных
+# 1. Скачать файл https://drive.google.com/file/d/1l5sia-9c-t91iIPiGyBc1s9mQ8RgTNqb/view?usp=sharing
+#       (обратите внимание на значок скачивания в правом верхнем углу,
+#       см https://drive.google.com/file/d/1M6mW1jI2RdZhdSCEmlbFi5eoAXOR3u6G/view?usp=sharing)
+# 2. Раззиповать средствами операционной системы содержимое архива
+#       в папку python_base/lesson_012/trades
+# 3. В каждом файле в папке trades содержится данные по сделакам по одному тикеру, разделенные запятыми.
+#   Первая строка - название колонок:
+#       SECID - тикер
+#       TRADETIME - время сделки
+#       PRICE - цена сделки
+#       QUANTITY - количество бумаг в этой сделке
+#   Все последующие строки в файле - данные о сделках
+#
+# Подсказка: нужно последовательно открывать каждый файл, вычитывать данные, высчитывать волатильность и запоминать.
+# Вывод на консоль можно сделать только после обработки всех файлов.
+#
+# Для плавного перехода к мультипоточности, код оформить в обьектном стиле, используя следующий каркас
+#
+# class <Название класса>:
+#
+#     def __init__(self, <параметры>):
+#         <сохранение параметров>
+#
+#     def run(self):
+#         <обработка данных>
+
+
+import os
+import time
+from operator import itemgetter
+from pathlib import Path
+
+
+class Ticker:
+
+    def __init__(self, file_name):
+        self.file = file_name
+        self.volatility = 0
+        self.name = ""
+
+    def run(self):
+        with open(self.file, 'r') as ff:
+            next(ff)
+            row = next(ff)
+            self.name, trade, price, quantity = row.split(",")
+            min_price, max_price = float(price), float(price)
+            for line in ff:
+                name, trade, price, quantity = line.split(",")
+                cost = float(price)
+                if cost > max_price:
+                    max_price = cost
+                if cost < min_price:
+                    min_price = cost
+            half_sum = (min_price + max_price) / 2
+            self.volatility = round(((max_price - min_price) / half_sum) * 100, 2)
+
+
+def generation_filename(path_file):
+    for dirpath, dirnames, filenames in os.walk(path_file):
+        for f in filenames:
+            file_path = os.path.join(dirpath, f)
+            yield file_path
+
+
+def sort_ticker(ticker_volatility):
+    tickers_nil = []
+    tickers_data_sort = sorted(ticker_volatility.items(), key=itemgetter(1))
+    index_nil = 0
+    for tick in tickers_data_sort:
+        if tick[1] == 0:
+            tickers_nil.append(tick[0])
+        else:
+            index_nil = tickers_data_sort.index(tick)
+            break
+    for i in range(1, 4):
+        print(tickers_data_sort[-i][0], tickers_data_sort[-i][1])
+    for i in range(index_nil + 2, index_nil - 1, -1):
+        print(tickers_data_sort[i][0], tickers_data_sort[i][1])
+    print("\nНулевая волатильность:")
+    for tick in sorted(tickers_nil):
+        print(f"{tick}", end=" ")
+
+
+def time_track(func):
+    def surrogate(*args, **kwargs):
+        started_at = time.time()
+        result = func(*args, **kwargs)
+        ended_at = time.time()
+        elapsed = round(ended_at - started_at, 4)
+        print(f'\n\nФункция работала {elapsed} секунд(ы)')
+        return result
+    return surrogate
+
+
+@time_track
+def main(path_ticker):
+    files = generation_filename(path_file=path_ticker)
+    tickers_data = {}
+    for file in files:
+        ticker = Ticker(file)
+        ticker.run()
+        tickers_data[ticker.name] = ticker.volatility
+    sort_ticker(ticker_volatility=tickers_data)
+
+
+path = Path.cwd()/"trades"
+if __name__ == '__main__':
+    main(path_ticker=path)
